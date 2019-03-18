aad82889ba71fc4793658dc8a9b2100e1a786ccb
Tomas Val <tomas.val@wework.com>
2019-03-18 19:03:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks() {
+function distanceFromHqInBlocks(block) {
   let hqBlock = 42;
   
-   
-  
+   return (block - hqBlock)
   
 }
